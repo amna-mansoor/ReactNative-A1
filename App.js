84fcc40b358154e25511e9d52b460856abec3283
amna mansoor <amnamansoor2468@gmail.com>
2026-06-07 21:02:45 +0500

    a1

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import 'react-native-gesture-handler'; // MUST BE AT THE TOP
+import 'react-native-gesture-handler'; 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
